8f7f1261eeea4546b72c12190cd139a9c42e3625
Thuwarakesh <thuwarakesh@gmail.com>
2019-03-02 11:36:25 +0530

    new styles

--- project/src/home/script.js
+++ project/src/home/script.js
@@ -1,1 +1,13 @@
 require('./style.scss');
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+export class Home extends Component {
+  render() {
+    return (
+      <p className="subtitle">Home</p>
+    )
+  }
+}
+
+ReactDOM.render(<Home/>, document.getElementById('home'));
--- project/src/script.js
+++ project/src/script.js
@@ -1,1 +1,13 @@
-require('./style.scss');
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+require('./style.scss')
+
+export class Base extends Component {
+  render() {
+    return (
+      <div className="title">Base</div>
+    )
+  }
+}
+
+ReactDOM.render(<Base/>, document.getElementById('base'));
